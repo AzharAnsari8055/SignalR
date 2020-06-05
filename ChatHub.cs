cd040b54eef7490a01e3e53d5a66c87a2da48c4c
AzharAnsari8055 <30493817+AzharAnsari8055@users.noreply.github.com>
2020-06-05 18:03:50 +0530

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        static List<Users> ConnectedUsers = new List<Users>();
+        static List<Messages> CurrentMessage = new List<Messages>();
+        ConnClass ConnC = new ConnClass();
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                string UserImg = GetUserImage(userName);
+                string logintime = DateTime.Now.ToString();
+
+                int userID = ConnC.GetUserIDByEmail("Select ID from tbl_Users where UserName = '" + userName + "'");
+
+                ConnectedUsers.Add(new Users { ConnectionId = id, UserName = userName, UserImage = UserImg, LoginTime = logintime });
+
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage, userID);
+
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName, UserImg, logintime);
+            }
+        }
+
+         
+
+        public void SendMessageToAll(string userName, string message, string time)
+        {
+            string UserImg = GetUserImage(userName);
+            // store last 100 messages in cache
+            AddMessageinCache(userName, message, time, UserImg);
+
+            // Broad cast message
+            Clients.All.messageReceived(userName, message, time, UserImg);
+
+        }
+
+        public void SendPrivateMessage(string toUserId, string message, string time, string userName)
+        {
+            try
+            {
+                string fromconnectionid = Context.ConnectionId;
+                string strfromUserId = (ConnectedUsers.Where(u => u.ConnectionId == Context.ConnectionId).Select(u => u.UserID).FirstOrDefault()).ToString();
+                int _fromUserId = 0;
+                int.TryParse(strfromUserId, out _fromUserId);
+                int _toUserId = 0;
+                int.TryParse(toUserId, out _toUserId);
+                List<Users> FromUsers = ConnectedUsers.Where(u => u.UserID == _fromUserId).ToList();
+                List<Users> ToUsers = ConnectedUsers.Where(x => x.UserID == _toUserId).ToList();
+
+                if (FromUsers.Count != 0 && ToUsers.Count() != 0)
+                {
+                    foreach (var ToUser in ToUsers)
+                    {
+                        // send to                                                                                            //Chat Title
+                        Clients.Client(ToUser.ConnectionId).sendPrivateMessage(_fromUserId.ToString(), FromUsers[0].UserName, FromUsers[0].UserName, message);
+                    }
+
+
+                    foreach (var FromUser in FromUsers)
+                    {
+                        // send to caller user                                                                                //Chat Title
+                        Clients.Client(FromUser.ConnectionId).sendPrivateMessage(_toUserId.ToString(), FromUsers[0].UserName, ToUsers[0].UserName, message);
+                    }
+                    // send to caller user
+                    //Clients.Caller.sendPrivateMessage(_toUserId.ToString(), FromUsers[0].UserName, message);
+                    //ChatDB.Instance.SaveChatHistory(_fromUserId, _toUserId, message);
+                    string UserImg = GetUserImage(userName);
+                    //MessageDetail _MessageDeail = new MessageDetail { FromUserName = FromUsers[0].UserName, Message = message, Time = time, UserImg = UserImg };
+                    ////MessageDetail _MessageDeail = new MessageDetail { FromUserID = _fromUserId, FromUserName = FromUsers[0].UserName, ToUserID = _toUserId, ToUserName = ToUsers[0].UserName, Message = message };
+                    //AddMessageinCache(_MessageDeail);
+                    //Clients.Client(fromconnectionid).addNewMessageToPage(userName, message);
+                    //Clients.All.messageReceived(userName, message, time, UserImg);
+                }
+            }
+            catch { }
+        }
+
+        private void AddMessageinCache(string userName, string message, string time, string UserImg)
+        {
+            CurrentMessage.Add(new Messages { UserName = userName, Message = message, Time = time, UserImage = UserImg });
+
+            if (CurrentMessage.Count > 100)
+                CurrentMessage.RemoveAt(0);
+
+            // Refresh();
+        }
+
+        public string GetUserImage(string username)
+        {
+            string RetimgName = "images/dummy.png";
+            try
+            {
+                string query = "select Photo from tbl_Users where UserName='" + username + "'";
+                string ImageName = ConnC.GetColumnVal(query, "Photo");
+
+                if (ImageName != "")
+                    RetimgName = "images/DP/" + ImageName;
+            }
+            catch (Exception ex)
+            { }
+            return RetimgName;
+        }
+
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                ConnectedUsers.Remove(item);
+
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
