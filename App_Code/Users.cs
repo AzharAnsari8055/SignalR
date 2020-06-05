e341f574940352de093da3b5cd6121d25c63dbfa
AzharAnsari8055 <30493817+AzharAnsari8055@users.noreply.github.com>
2020-06-05 17:40:15 +0530

    Add files via upload

---
+++ App_Code/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat
+{
+    public class Users
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+        public string UserImage { get; set; }
+        public string LoginTime { get; set; }
+        public int UserID { get; set; }
+    }
+
+    public class MessageDetail
+    {
+        public int FromUserID { get; set; }
+        public string FromUserName { get; set; }
+        public int ToUserID { get; set; }
+        public string ToUserName { get; set; }
+        public string Message { get; set; }
+        public string Time { get; set; }
+        public string UserImg { get; set; }
+    }
+}
